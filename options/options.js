adf56544d19c6f5865f89d4e2a419bab1313da2b
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 23:16:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-frost-dev-133395",
+  url: "https://yellow-frost-dev-133395.botics.co",
+  api: "https://yellow-frost-dev-133395.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
